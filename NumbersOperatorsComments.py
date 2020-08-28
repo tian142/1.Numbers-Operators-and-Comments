f35b1da8d5ab2da61083b2abbf6ff64947c0ed68
Tian Qiu <tian.qiu142@gmail.com>
2020-08-28 13:41:24 -0700

    first commit
    sadadadsafas

--- NumbersOperatorsComments.py
+++ NumbersOperatorsComments.py
@@ -3,3 +3,4 @@
 # modulo(%) remainder operator, divide as many times as possible, output is that remains
 # interger division(//) do the devision, give interget(whole number rounded down) as result
 print(1 + 2)
+# test
